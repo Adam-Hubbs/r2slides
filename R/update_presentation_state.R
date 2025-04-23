202ac70ecfe30ac456d3dfa572bc0f555c65c061
Adam Hubbs <amhubbs17@gmail.com>
2025-04-23 12:03:10 -0600

    skeleton update_presentation

---
+++ R/update_presentation_state.R
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+update_presentation_state <- function(presentation_id) {
+  # Update slide ids
+
+
+  # Update presentation Name
+
+
+  # Update page size
+
+
+  # Print to console anything that has changed
+
+  # All of these depend on a GET call to query2
+}
+
+
+
+has_updates <- function(presentation_id) {
+  # Check that the revision_id is the same
+  # All of these depend on a GET call to query2
+}
