faa85ef92faa4fba843b3110f6c0dbf9d60625e4
Adam Hubbs <amhubbs17@gmail.com>
2026-02-03 12:53:27 -0700

    Support R 4.3+

--- R/aaa.R
+++ R/aaa.R
@@ -1,10 +1,6 @@
 #' @keywords internal
 "_PACKAGE"
 
-# enable usage of <S7_object>@name in package code
-#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
-NULL
-
 ## usethis namespace: start
 #' @importFrom httr2 obfuscated
 ## usethis namespace: end
